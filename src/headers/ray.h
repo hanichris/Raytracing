fcd0ad89b5085e5e441e51343a4e8eec11c5e70e
hanichris <hanichris71@gmail.com>
2025-01-09 23:26:37 +0300

    Find the value of a ray tracer at a given time scale.

---
+++ src/headers/ray.h
@@ -0,0 +1,26 @@
+#ifndef MY_RAY_H
+#define MY_RAY_H 1
+
+#include "vec3.h"
+
+typedef struct ray ray;
+struct ray {
+	point3 orig;
+	vec3 dir;
+};
+
+static
+point3* at(ray const* r, double t, point3* out) {
+	if (r) {
+		vec3 tmp = {
+			.x = r->dir.x,
+			.y = r->dir.y,
+			.z = r->dir.z
+		};
+		add(&r->orig, scalar_mul(&tmp, t), out);
+	}
+	return out;
+}
+
+
+#endif
