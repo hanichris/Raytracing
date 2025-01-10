5ceb14bb0f7b7429931e6c849046969d59aef901
hanichris <hanichris71@gmail.com>
2025-01-10 17:03:35 +0300

    Transform the interfaces for unit_vec3 and cross to have a const modifier and use an output struct to store the result of the computation.

--- src/vec3.c
+++ src/vec3.c
@@ -1,7 +1,7 @@
 #include "headers/vec3.h"
 
-vec3* unit_vec3(vec3* u) {
-	return u ? scalar_div(u, len(u)) : u;
+vec3* unit_vec3(vec3 const* u, vec3* out) {
+	return u && out ? scalar_div(u, len(u), out) : nullptr;
 }
 
 vec3* sub(vec3 const* u, vec3 const* v, vec3* out) {
@@ -24,7 +24,7 @@ vec3* add(vec3 const* u, vec3 const* v, vec3* out) {
 	return nullptr;
 }
 
-vec3* cross(vec3* u, vec3* v, vec3* out) {
+vec3* cross(vec3 const* u, vec3 const* v, vec3* out) {
 	if (u && v && out) {
 		out->x = (u->y * v->z) - (u->z * v->y);
 		out->y = (u->z * v->x) - (u->x * v->z);
