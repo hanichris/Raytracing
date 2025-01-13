1e89a099a437090456d60725c1d834b99927183f
hanichris <hanichris71@gmail.com>
2025-01-13 21:22:24 +0300

    test the hadamard multiplication functionality.

--- test/test_col3.c
+++ test/test_col3.c
@@ -28,6 +28,8 @@ void test_colour_addition(void) {
 	assert(float_equal(out->red, 1.6));
 	assert(float_equal(out->green, 0.7));
 	assert(float_equal(out->blue, 1.0));
+
+	putchar('.');
 }
 
 static
@@ -39,23 +41,40 @@ void test_colour_subtraction(void) {
 	assert(float_equal(out->red, 0.2));
 	assert(float_equal(out->green, 0.5));
 	assert(float_equal(out->blue, 0.5));
+
+	putchar('.');
 }
 
 static
-void test_colour_multiplication(void) {
+void test_colour_scalar_multiplication(void) {
 	col3* c = COLOUR(0.9, 0.6, 0.75);
 
 	col3* out = COL3_MUL(c, 2);
 	assert(float_equal(out->red, 1.8));
 	assert(float_equal(out->green, 1.2));
 	assert(float_equal(out->blue, 1.5));
+
+	putchar('.');
 }
 
+static
+void test_colour_hadamard_multiplication(void) {
+	col3* c = COLOUR(1, 0.2, 0.4);
+	col3* d = COLOUR(0.9, 1, 0.1);
+
+	col3* out = COL3_HADAMARD(c, d);
+	assert(float_equal(out->red, 0.9));
+	assert(float_equal(out->green, 0.2));
+	assert(float_equal(out->blue, 0.04));
+
+	putchar('.');
+}
 
 void run_col3_tests(void) {
 	test_colour_creation();
 	test_colour_addition();
 	test_colour_subtraction();
-	test_colour_multiplication();
+	test_colour_scalar_multiplication();
+	test_colour_hadamard_multiplication();
 }
 
