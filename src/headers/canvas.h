608dc474cf54fc569e0f6ac2cfea84b02a225a5c
hanichris <hanichris71@gmail.com>
2025-01-16 03:55:37 +0300

    define the canvas.h functions in their own translation units.

--- src/headers/canvas.h
+++ src/headers/canvas.h
@@ -13,6 +13,8 @@
 
 #include "colour.h"
 
+#define MAX_COL_VAL 255
+
 typedef struct canvas canvas;
 struct canvas {
 	uint16_t width;
@@ -23,16 +25,7 @@ struct canvas {
  * canvas_init - initialises a canvas buffer with maximally `w` * `h` pixels.
  * Only use this function on an uninitialised canvas buffer.
  */
-static
-canvas* canvas_init(canvas *c, uint16_t w, uint16_t h) {
-	if (c) {
-		c->height = h;
-		c->width = w;
-		for (uint16_t i = 0; i < (w*h); i++)
-			c->pixels[i] = (col3){ };
-	}
-	return c;
-}
+canvas* canvas_init(canvas *c, uint16_t w, uint16_t h);
 
 /**
  * write_pixel - writes a pixel to the canvas at the specified (x,y) position.
@@ -42,14 +35,7 @@ canvas* canvas_init(canvas *c, uint16_t w, uint16_t h) {
  * @colour: pointer to the pixel colour.
  * @Returns: modified canvas or null.
  */
-static
-canvas* write_pixel(canvas* c, uint16_t x, uint16_t y, col3* colour) {
-	if (c && colour) {
-		c->pixels[(x * c->width) + y] = *colour;
-		return c;
-	}
-	return nullptr;
-}
+canvas* write_pixel(canvas* c, uint16_t x, uint16_t y, col3* colour);
 
 /**
  * pixel_at - returns the address of the pixel at the given (x, y) position.
@@ -58,12 +44,16 @@ canvas* write_pixel(canvas* c, uint16_t x, uint16_t y, col3* colour) {
  * @y: y-coordinate on the canvas. Goes from top to bottom.
  * @Returns: pixel's address or null.
  */
-static
-col3 const* pixel_at(canvas const* c, uint16_t x, uint16_t y) {
-	if (c && x < c->width && y < c->height)
-		return &c->pixels[(x * c->width) + y];
-	return nullptr;
-}
+col3 const* pixel_at(canvas const* c, uint16_t x, uint16_t y);
+
+/**
+ * canvas_2_ppm - generates a string representation of the canvas
+ * that will be written out to a file with the ppm format.
+ * @c: pointer to the canvas.
+ * @Returns: string buffer holding the canvas information.
+ * Otherwise, null if the string buffer could not be allocated.
+ */
+char* canvas_2_ppm(canvas* c);
 
 /**
  * canvas_delete - delete a canvas buffer `c`. The buffer must have been
