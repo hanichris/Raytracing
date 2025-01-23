ae0bc8d36996c157b60acaf0e87c6bdf43407f8d
hanichris <hanichris71@gmail.com>
2025-01-23 19:14:52 +0300

    test the functionality of matrix inversion, computing the determinant of a 3x3 and 4x4 matrix, and testing the invertibility of a matrix.

--- test/test_matrix.c
+++ test/test_matrix.c
@@ -388,6 +388,186 @@ void test_mat_cofactor_of_3x3_matrix(void) {
 	putchar('.');
 }
 
+static
+void test_mat_3x3_determinant(void) {
+	mat9 a = {
+		.m00=1, .m01=2, .m02=6,
+		.m10=-5,.m11=8, .m12=-4,
+		.m20=2, .m21=6, .m22=4,
+	};
+
+	assert(float_equal(mat9_determinant(&a), -196));
+
+	putchar('.');
+}
+
+static
+void test_mat_minor_of_4x4_matrix(void) {
+	mat16 a = {
+		.m00=-2, .m01=-8,.m02=3, .m03=5,
+		.m10=-3, .m11=1, .m12=7, .m13=3,
+		.m20=1,  .m21=2, .m22=-9,.m23=6,
+		.m30=-6, .m31=7, .m32=7, .m33=-9,
+	};
+
+	assert(float_equal(mat16_minor(&a, 0, 0), 690));
+	assert(float_equal(mat16_minor(&a, 0, 1), -447));
+	assert(float_equal(mat16_minor(&a, 0, 2), 210));
+	assert(float_equal(mat16_minor(&a, 0, 3), -51));
+
+	putchar('.');
+}
+
+static
+void test_mat_cofactor_of_4x4_matrix(void) {
+	mat16 a = {
+		.m00=-2, .m01=-8,.m02=3, .m03=5,
+		.m10=-3, .m11=1, .m12=7, .m13=3,
+		.m20=1,  .m21=2, .m22=-9,.m23=6,
+		.m30=-6, .m31=7, .m32=7, .m33=-9,
+	};
+
+	assert(float_equal(mat16_cofactor(&a, 0, 0), 690));
+	assert(float_equal(mat16_cofactor(&a, 0, 1), 447));
+	assert(float_equal(mat16_cofactor(&a, 0, 2), 210));
+	assert(float_equal(mat16_cofactor(&a, 0, 3), 51));
+
+	putchar('.');
+}
+
+static
+void test_mat_4x4_determinant(void) {
+	mat16 a = {
+		.m00=-2, .m01=-8,.m02=3, .m03=5,
+		.m10=-3, .m11=1, .m12=7, .m13=3,
+		.m20=1,  .m21=2, .m22=-9,.m23=6,
+		.m30=-6, .m31=7, .m32=7, .m33=-9,
+	};
+
+	assert(float_equal(mat16_determinant(&a), -4071));
+
+	putchar('.');
+}
+
+static
+void test_mat_4x4_matrix_is_invertible(void) {
+	mat16 a = {
+		.m00=6, .m01=4, .m02=4, .m03=4,
+		.m10=5, .m11=5, .m12=7, .m13=6,
+		.m20=4, .m21=-9,.m22=3, .m23=-7,
+		.m30=9, .m31=1, .m32=7, .m33=-6,
+	};
+
+	assert(!float_equal(mat16_determinant(&a), 0));
+
+	putchar('.');
+}
+
+static
+void test_mat_4x4_matrix_is_not_invertible(void) {
+	mat16 a = {
+		.m00=-4,.m01=2, .m02=-2,.m03=-3,
+		.m10=9, .m11=6, .m12=2, .m13=6,
+		.m20=0, .m21=-5,.m22=1, .m23=-5,
+		.m30=0, .m31=0, .m32=0, .m33=0,
+	};
+
+	assert(float_equal(mat16_determinant(&a), 0));
+
+	putchar('.');
+}
+
+static
+void test_mat_invert_4x4_matrix_1(void) {
+	mat16 a = {
+		.m00=-5,.m01=2, .m02=6, .m03=-8,
+		.m10=1, .m11=-5,.m12=1, .m13=8,
+		.m20=7, .m21=7, .m22=-6,.m23=-7,
+		.m30=1, .m31=-3,.m32=7, .m33=4,
+	};
+
+	mat16 b = {
+		.m00=0.21805, .m01=0.45113, .m02=0.24060, .m03=-0.04511,
+		.m10=-0.80827,.m11=-1.45677,.m12=-0.44361,.m13=0.52068,
+		.m20=-0.07895,.m21=-0.22368,.m22=-0.05263,.m23=0.19737,
+		.m30=-0.52256,.m31=-0.81391,.m32=-0.30075,.m33=0.30639,
+	};
+
+	mat16* out = MAT16_INVERSE(&a);
+
+	assert(float_equal(mat16_determinant(&a), 532));
+	assert(float_equal(mat16_cofactor(&a, 2, 3), -160));
+	assert(float_equal(mat16_cofactor(&a, 3, 2), 105));
+	assert(mat16_is_equal(out, &b));
+
+	putchar('.');
+}
+
+static
+void test_mat_invert_4x4_matrix_2(void) {
+	mat16 a = {
+		.m00=8, .m01=-5,.m02=9, .m03=2,
+		.m10=7, .m11=5, .m12=6, .m13=1,
+		.m20=-6,.m21=0, .m22=9, .m23=6,
+		.m30=-3,.m31=0, .m32=-9,.m33=-4,
+	};
+
+	mat16 b = {
+		.m00=-0.15385, .m01=-0.15385, .m02=-0.28205, .m03=-0.53846,
+		.m10=-0.07692, .m11= 0.12308, .m12= 0.02564, .m13= 0.03077,
+		.m20= 0.35897, .m21= 0.35897, .m22= 0.43590, .m23= 0.92308,
+		.m30=-0.69231, .m31=-0.69231, .m32=-0.76923, .m33=-1.92308,
+	};
+
+	assert(mat16_is_equal(MAT16_INVERSE(&a), &b));
+
+	putchar('.');
+}
+
+static
+void test_mat_invert_4x4_matrix_3(void) {
+	mat16 a = {
+		.m00= 9, .m01= 3, .m02= 0, .m03= 9,
+		.m10=-5, .m11=-2, .m12=-6, .m13=-3,
+		.m20=-4, .m21= 9, .m22= 6, .m23= 4,
+		.m30=-7, .m31= 6, .m32= 6, .m33= 2,
+	};
+
+	mat16 b = {
+		.m00=-0.04074, .m01=-0.07778, .m02= 0.14444, .m03=-0.22222,
+		.m10=-0.07778, .m11= 0.03333, .m12= 0.36667, .m13=-0.33333,
+		.m20=-0.02901, .m21=-0.14630, .m22=-0.10926, .m23= 0.12963,
+		.m30= 0.17778, .m31= 0.06667, .m32=-0.26667, .m33= 0.33333,
+	};
+
+	assert(mat16_is_equal(MAT16_INVERSE(&a), &b));
+
+	putchar('.');
+}
+
+static
+void test_mat_multiply_a_product_by_its_inverse(void) {
+	mat16 a = {
+		.m00= 3, .m01=-9, .m02= 7, .m03= 3,
+		.m10= 3, .m11=-8, .m12= 2, .m13=-9,
+		.m20=-4, .m21= 4, .m22= 4, .m23= 1,
+		.m30=-6, .m31= 5, .m32=-1, .m33= 1,
+	};
+
+	mat16 b = {
+		.m00= 8, .m01= 2, .m02= 2, .m03= 2,
+		.m10= 3, .m11=-1, .m12= 7, .m13= 0,
+		.m20= 7, .m21= 0, .m22= 5, .m23= 4,
+		.m30= 6, .m31=-2, .m32= 0, .m33= 5,
+	};
+	
+	mat16* c = MAT16_MUL(&a, &b);
+
+	assert(mat16_is_equal(MAT16_MUL(c, MAT16_INVERSE(&b)), &a));
+
+	putchar('.');
+}
+
 void run_mat_tests(void) {
 	test_mat_create_4x4_matrix();
 	test_mat_create_3x3_matrix();
@@ -413,6 +593,16 @@ void run_mat_tests(void) {
 	test_mat_create_2x2_submatrix_of_3x3_matrix_null();
 	test_mat_minor_of_3x3_matrix();
 	test_mat_cofactor_of_3x3_matrix();
+	test_mat_3x3_determinant();
+	test_mat_minor_of_4x4_matrix();
+	test_mat_cofactor_of_4x4_matrix();
+	test_mat_4x4_determinant();
+	test_mat_4x4_matrix_is_invertible();
+	test_mat_4x4_matrix_is_not_invertible();
+	test_mat_invert_4x4_matrix_1();
+	test_mat_invert_4x4_matrix_2();
+	test_mat_invert_4x4_matrix_3();
+	test_mat_multiply_a_product_by_its_inverse();
 }
 
 #undef EPSILON
